eba8378a92ab93fd8554cd1265b51419329e0c99
Edward Nilsson <en222yu@student.lnu.se>
2023-03-07 21:45:07 -0600

    Testing character model with strawberry before db connection

--- fastapi-graphql/main.py
+++ fastapi-graphql/main.py
@@ -5,10 +5,25 @@ from strawberry.fastapi import GraphQLRouter
 
 
 @strawberry.type
+class Character:
+    id: int
+    name: str
+    # status: str
+    # species: str
+    # type: str 
+    # gender: str 
+    # origin: object 
+    # location: object 
+    # image: str 
+    # episodes: object
+    # url: str
+    # created: str
+
+@strawberry.type
 class Query:
     @strawberry.field
-    def hello(self) -> str:
-        return "Hello World"
+    def character(self) -> Character:
+        return Character(id=1, name="Rick")
 
 schema = strawberry.Schema(Query)
 
