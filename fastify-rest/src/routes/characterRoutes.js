1ed3ba0cbf7459efe4e2efa32526c21d6bca415c
Edward Nilsson <en222yu@student.lnu.se>
2023-03-09 22:27:04 -0600

    Revert "GraphQL succesfully connected to db"
    This reverts commit 9df973e13dd6da2b529d272b28e47aec7a7a7b3d.

--- fastify-rest/src/routes/characterRoutes.js
+++
@@ -1,7 +0,0 @@
-const characterController = require('../controllers/characterController')
-
-module.exports = (app) => {
-    app.get('/api/characters', characterController.getCharacters)
-
-    app.get('/api/characters/:id', characterController.getCharacter)
-}
