c8e88947d81e10e04bbb7bce3af42feebbd80e94
Edward Nilsson <en222yu@student.lnu.se>
2023-05-08 23:35:15 -0600

    requirements for installation

--- python/flask-rest/database.py
+++ python/flask-rest/database.py
@@ -6,7 +6,6 @@ client = pymongo.MongoClient(os.environ['MONGO_URI'])
 db = client.RestThesisAPI
 collection = db.characters
 
-
 def fetch_one_character(id):
     document = collection.find_one({"id": int(id)})
     if document:
@@ -19,23 +18,3 @@ def fetch_all_characters():
     for document in cursor:
         characters.append(Character(**document))
     return characters
-
-def create_character(character):
-    document = character.dict()
-    result = collection.insert_one(document)
-    return Character(**document)
-
-def update_character(id, status, image, url):
-    collection.update_one(
-        {"id": int(id)}, 
-        {"$set": {"status": status, "image": image, "url": url}},
-        upsert=True
-    )
-    document = collection.find_one({"id": int(id)})
-    if document:
-        return Character(**document)
-    return None
-
-def remove_character(id):
-    result = collection.delete_one({"id": int(id)})
-    return result.deleted_count > 0
